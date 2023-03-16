73114c4d965dd4f354c3b3df5088643b685ce0ad
hajar <hajar.elrh@gmail.com>
2023-03-16 12:45:39 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Using puts function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle\n");
+	return (0);
+}
