7e4254b7a2047d99ebc0a1694986889c6d255fd9
hajar <hajar.elrh@gmail.com>
2023-03-17 15:23:23 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,18 +9,18 @@ int main(void)
 	int m;
 for (n = 0 ; n <= 9 ; n++)
 {
-	for (m = 1 ; m <= 9 ; m++)
+for (m = 1 ; m <= 9 ; m++)
 {
-	if (n < m && n != m)
-	{
-		putchar(n + '0');
-		putchar(m + '0');
+if (n < m && n != m)
+{
+putchar(n + '0');
+putchar(m + '0');
 if (n + m != 17)
 {
-		putchar(',');
-		putchar(' ');
+putchar(',');
+putchar(' ');
 }
-	}
+}
 }
 }
 putchar('\n');
