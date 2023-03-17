43f587d5ab593a8a4580e5e536553c0e59c84ec2
hajar <hajar.elrh@gmail.com>
2023-03-17 14:38:40 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,13 @@ int main(void)
 	int n;
 for (n = 0 ; n <= 9 ; n++)
 {
+putchar(n + '0');
 if (n == 9)
-
-putchar(n + '0');
-else
 {
-putchar(n + '0');
+	continue;
+}
 putchar(' ');
 putchar(',');
-}
 }
 putchar('\n');
 return (0);
