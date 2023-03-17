30c10417e3d2585f7b49a2972c0624af3d8a8c48
hajar <hajar.elrh@gmail.com>
2023-03-17 12:16:38 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10
+ * Starting from 0
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n = 0
+	while (n <= 9)
+{
+	putchar(n);
+	n++;
+}
+putchar ('\n');
+return (0);
+}
+	
