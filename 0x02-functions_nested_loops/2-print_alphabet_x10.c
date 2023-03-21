18abec03fcade9b9383fe727848b29dbd77395c4
hajar <hajar.elrh@gmail.com>
2023-03-21 15:08:56 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Function that prints x10 the alphabet
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+char i;
+int j; 
+j = 0;
+while (j < 10)
+{
+for (i = 'a' ; i <= 'z' ; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+j++;
+}
+}
