9bcd1eab9ad691866083057796e111c37e0c3c1e
hajar <hajar.elrh@gmail.com>
2023-03-21 17:46:33 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Program that computes and prints the sum of all multiples 3/5
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int a;
+int sum;
+sum = 0;
+for (a = 0; a < 10 ; a++)
+{
+if ((a % 3 == 0) || (a % 5 == 0))
+{
+sum = sum + a;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
