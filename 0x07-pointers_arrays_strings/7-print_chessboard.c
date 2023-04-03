788e1f53888ff57384963f42ce4e4f7550f32abc
hajar <hajar.elrh@gmail.com>
2023-04-03 10:58:56 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_chessboard - Function that print the chessboard
+ * @a: Array
+sh: 1: q: not found
+ */
+void print_chessboard(char (*a)[8])
+{
+int n;
+int m;
+for (n = 0 ; n < 8 ; n++)
+{
+for (m = 0 ; m < 8 ; m++)
+_putchar(a[n][m]);
+_putchar('\n');
+}
+}
