1eb2fc185b45286d7838529a897ea6a8da1e0592
hajar <hajar.elrh@gmail.com>
2023-04-17 07:37:36 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that prints the name of the file it was compiled
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
